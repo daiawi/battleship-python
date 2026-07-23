8ce2816e7f89329260068c01a621935c146b75ef
daniel <daiawi972@gmail.com>
2026-07-23 13:24:19 -0400

    Cell input with validation

--- src/battleship/core/game.py
+++ src/battleship/core/game.py
@@ -3,7 +3,9 @@ from battleship.core.board import Board
 
 class Game:
     def __init__(self):
-        self.board = Board(10)
+        self.size = 10
+        self.board = Board(self.size)
+        self.instruction = "Place Ships!"
 
     def is_over(self):
         return False
--- src/battleship/ui/console.py
+++ src/battleship/ui/console.py
@@ -6,10 +6,13 @@ from battleship.core.ship import Ship
 class ConsoleUI:
     def __init__(self, game: Game):
         self.game = game
+        self.letters = [chr(x + 65) for x in range(self.game.size)]
+        self.numbers = [str(x) for x in range(self.game.size)]
 
     def run(self):
         while not self.game.is_over():
             self.display()
+            self.take_input()
             break
 
     def display(self):
@@ -19,13 +22,45 @@ class ConsoleUI:
         board = self.game.get_board()
         print(self.board_to_str(board))
         print("=" * 22)
+
+    def take_input(self):
+        print(self.game.instruction)
+
+        while True:
+            cell_code = input("Input Cell: ")
+
+            if self.validate_input_cell(cell_code):
+                break
+
+            print("Sorry, try again!")
+
+        cell = self.parse_input_cell(cell_code) 
+        print(cell)
+
+
+    def validate_input_cell(self, cell_code: str) -> bool:
+        if not cell_code:
+            return False
+
+        letter_valid = cell_code[0].upper() in self.letters
+        number_valid = cell_code[1:] in self.numbers
         
+        return letter_valid and number_valid
+
+    def parse_input_cell(self, cell_code: str) -> tuple[int, int]:
+        let_str = cell_code[0].upper()
+        num_str = cell_code[1]
+
+        row = self.letters.index(let_str)
+        col = self.numbers.index(num_str)
+
+        return (row, col)
+
     def board_to_str(self, board: Board) -> str:
         output = []
-        letters = [chr(x + 65) for x in range(board.size)]
 
         for row in range(board.size):
-            line = [letters[row]]
+            line = [self.letters[row]]
 
             for col in range(board.size):
                 cell = (row, col)
@@ -37,7 +72,7 @@ class ConsoleUI:
 
             output.append(" ".join(line))
 
-        col_nums = [str(x) for x in range(board.size)]
+        col_nums = self.numbers.copy()
         col_nums.insert(0," ")
         output.append(" ".join(col_nums))
 
