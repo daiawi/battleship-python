2ef43bf4a52f9a5104c119fb2b19b8b0e4ce8ff9
daniel <daiawi972@gmail.com>
2026-07-23 13:38:31 -0400

    Abstract Cell Input to it's own Class

---
+++ src/battleship/ui/cell_input.py
@@ -0,0 +1,39 @@
+from battleship.core.game import Game
+
+
+class CellInput:
+    def __init__(self, game: Game, letters, numbers) -> None:
+        self.game = game
+        self.letters = letters
+        self.numbers = numbers
+
+    def take_input(self) -> tuple[int, int]:
+            print(self.game.instruction)
+    
+            while True:
+                cell_code = input("Input Cell: ")
+    
+                if self.validate_input_cell(cell_code):
+                    break
+    
+                print("Sorry, try again!")
+    
+            return self.parse_input_cell(cell_code) 
+
+    def validate_input_cell(self, cell_code: str) -> bool:
+        if not cell_code:
+            return False
+
+        letter_valid = cell_code[0].upper() in self.letters
+        number_valid = cell_code[1:] in self.numbers
+        
+        return letter_valid and number_valid
+
+    def parse_input_cell(self, cell_code: str) -> tuple[int, int]:
+        let_str = cell_code[0].upper()
+        num_str = cell_code[1]
+
+        row = self.letters.index(let_str)
+        col = self.numbers.index(num_str)
+
+        return (row, col)
--- src/battleship/ui/console.py
+++ src/battleship/ui/console.py
@@ -1,6 +1,7 @@
 from battleship.core.board import Board
 from battleship.core.game import Game
 from battleship.core.ship import Ship
+from battleship.ui.cell_input import CellInput
 
 
 class ConsoleUI:
@@ -8,11 +9,12 @@ class ConsoleUI:
         self.game = game
         self.letters = [chr(x + 65) for x in range(self.game.size)]
         self.numbers = [str(x) for x in range(self.game.size)]
+        self.input_handler = CellInput(self.game, self.letters, self.numbers)
 
     def run(self):
         while not self.game.is_over():
             self.display()
-            self.take_input()
+            self.input_handler.take_input()
             break
 
     def display(self):
@@ -22,39 +24,6 @@ class ConsoleUI:
         board = self.game.get_board()
         print(self.board_to_str(board))
         print("=" * 22)
-
-    def take_input(self):
-        print(self.game.instruction)
-
-        while True:
-            cell_code = input("Input Cell: ")
-
-            if self.validate_input_cell(cell_code):
-                break
-
-            print("Sorry, try again!")
-
-        cell = self.parse_input_cell(cell_code) 
-        print(cell)
-
-
-    def validate_input_cell(self, cell_code: str) -> bool:
-        if not cell_code:
-            return False
-
-        letter_valid = cell_code[0].upper() in self.letters
-        number_valid = cell_code[1:] in self.numbers
-        
-        return letter_valid and number_valid
-
-    def parse_input_cell(self, cell_code: str) -> tuple[int, int]:
-        let_str = cell_code[0].upper()
-        num_str = cell_code[1]
-
-        row = self.letters.index(let_str)
-        col = self.numbers.index(num_str)
-
-        return (row, col)
 
     def board_to_str(self, board: Board) -> str:
         output = []
