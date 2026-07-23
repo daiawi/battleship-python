dbfed2f2f5a55746e86bc6427bf14933d8dad0bb
daniel <daiawi972@gmail.com>
2026-07-23 01:42:27 -0400

    Oneshot Render

--- src/battleship/core/game.py
+++ src/battleship/core/game.py
@@ -1,3 +1,12 @@
+from battleship.core.board import Board
+
+
 class Game:
     def __init__(self):
-        pass
+        self.board = Board(10)
+
+    def is_over(self):
+        return False
+
+    def get_board(self):
+        return self.board
--- src/battleship/ui/console.py
+++ src/battleship/ui/console.py
@@ -1,9 +1,35 @@
 from battleship.core.game import Game
-
+from battleship.core.board import Board
+from battleship.core.ship import Ship
 
 class ConsoleUI:
     def __init__(self, game: Game):
         self.game = game
 
     def run(self):
-        pass
+        while not self.game.is_over():
+            self.display()
+            break
+
+    def display(self):
+        print("Battleship")
+
+        board = self.game.get_board()
+        print(self.board_to_str(board))
+        
+    def board_to_str(self, board: Board) -> str:
+        output = []
+
+        for row in range(board.size):
+            line = []
+
+            for col in range(board.size):
+                cell = (row, col)
+                if cell in board.ship_locations:
+                    line.append("S")
+                else:
+                    line.append("~")
+
+            output.append(" ".join(line))
+
+        return "\n".join(output)
